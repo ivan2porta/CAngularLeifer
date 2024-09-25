e57128037b7c98c0850642f0b6c00438bca1531d
ivan2porta <132475555+ivan2porta@users.noreply.github.com>
2024-09-25 12:13:06 +0200

    Creacion dataset

--- spotify/src/app/app.component.ts
+++ spotify/src/app/app.component.ts
@@ -7,31 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '';
-
-  status:string | number = 2;
-  
- car:CarModel = {
-  brand: 'Ford',
-  model: 'Focus',
-  year: 2021
- }
-
- listCars:Array<CarModel> = [
-  {
-    brand:'Porche',
-    model:'Carrera',
-    year:2022
-  },
-  {
-    brand:'Renault',
-    model:'Clio',
-    year: 2001
-  }
- ]
 }
 
-interface CarModel {
-  brand:string;
-  model:string;
-  year?:number;
-}
+
