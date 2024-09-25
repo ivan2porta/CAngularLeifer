b5691ecb20b4c12a5cce8faba347ae33f2825f70
ivan2porta <132475555+ivan2porta@users.noreply.github.com>
2024-09-25 11:04:32 +0200

    RepasoTS

--- spotify/src/app/app.component.ts
+++ spotify/src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spotify';
+  title = '';
+
+  status:string | number = 2;
+  
+ car:CarModel = {
+  brand: 'Ford',
+  model: 'Focus',
+  year: 2021
+ }
+
+ listCars:Array<CarModel> = [
+  {
+    brand:'Porche',
+    model:'Carrera',
+    year:2022
+  },
+  {
+    brand:'Renault',
+    model:'Clio',
+    year: 2001
+  }
+ ]
+}
+
+interface CarModel {
+  brand:string;
+  model:string;
+  year?:number;
 }
